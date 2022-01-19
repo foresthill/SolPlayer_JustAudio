698d9b5a01935b08ab4ad55d98ec5cdc4d53c662
foresthill <g1989n@gmail.com>
2022-01-20 06:58:28 +0900

    JustAudio導入（＋ビルド通すところまで）

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Player(),
     );
   }
 }
@@ -110,6 +113,107 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+/**
+ * Flutter full app 1. Music Player: create a simple Flutter music player app
+ * (Just Audio Tutorial)
+ *  https://ishouldgotosleep.com/posts/simple-flutter-music-player-app/
+ */
+class Player extends StatefulWidget {
+  @override
+  _PlayerState createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  AudioPlayer _audioPlayer = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    // _audioPlayer = AudioPlayer();
+
+    // Set a sequence of audio sources that will be played by the audio player
+    try {
+      _audioPlayer
+        .setAudioSource(ConcatenatingAudioSource(children: [
+          AudioSource.uri(Uri.parse(
+            "https://archive.org/download/IGM-V7/IGM%20-%20Vol.%207/25%20Diablo%20-%20Tristram%20%28Blizzard%29.mp3")),
+          AudioSource.uri(Uri.parse(
+            "https://archive.org/download/igm-v8_202101/IGM%20-%20Vol.%208/15%20Pokemon%20Red%20-%20Cerulean%20City%20%28Game%20Freak%29.mp3")),
+          AudioSource.uri(Uri.parse(
+            "https://scummbar.com/mi2/MI1-CD/01%20-%20Opening%20Themes%20-%20Introduction.mp3")),
+      ]))
+        .catchError((error) {
+          // catch load errors: 404, invalid url ..
+        print("An error occured $error");
+      });
+    } catch (e, s) {
+      print(s);
+    }
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Widget _playerButton(PlayerState? playerState) {
+
+    // 1
+    final processingState = playerState?.processingState;
+    if (processingState == ProcessingState.loading ||
+        processingState == ProcessingState.buffering) {
+
+      // 2
+      return Container(
+        margin: EdgeInsets.all(8.0),
+        width: 64.0,
+        height: 64.0,
+        child: CircularProgressIndicator(),
+      );
+    } else if (_audioPlayer.playing != true) {
+
+      // 3
+      return IconButton(
+        icon: Icon(Icons.play_arrow),
+        iconSize: 64.0,
+        onPressed: _audioPlayer.play,
+      );
+    } else if (processingState != ProcessingState.completed) {
+
+      // 4
+      return IconButton(
+        icon: Icon(Icons.pause),
+        iconSize: 64.0,
+        onPressed: _audioPlayer.pause,
+      );
+    } else {
+
+      // 5
+      return IconButton(
+        icon: Icon(Icons.replay),
+        iconSize: 64.0,
+        onPressed: () => _audioPlayer.seek(Duration.zero,
+            index: _audioPlayer.effectiveIndices.first),
+      );
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: StreamBuilder<PlayerState>(
+          stream: _audioPlayer.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            return _playerButton(playerState);
+          },
+        ),
+      ),
     );
   }
 }
